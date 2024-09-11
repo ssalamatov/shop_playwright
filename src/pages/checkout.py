b55a7f79faa3f444dfae9715618a13836dab37c4
Stanislav Salamatov <s.salamatov@gmail.com>
2024-09-11 20:02:07 +0300

    add remove test

--- src/pages/checkout.py
+++ src/pages/checkout.py
@@ -1,13 +1,21 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
+
 
 
 class Checkout:
     def __init__(self, page: Page):
         self.page = page
 
-        self.basket_cards = '[data-qa="basket-card"]'
-        self.basket_item_price = '[data-qa="basket-item-price"]'
-        self.basket_counter = '[data-qa="basket-card-remove-item"]'
+        self.basket_cards = self.page.locator('[data-qa="basket-card"]')
+        self.basket_item_price = self.page.locator('[data-qa="basket-item-price"]')
+        self.basket_remove_item = self.page.locator('[data-qa="basket-card-remove-item"]')
 
-    def remove_product(self, product_id):
-        pass
+    def remove_product_from_basket(self, product_id: int):
+        self.basket_cards.wait_for()
+        before = self.basket_cards.count()
+
+        remove_button = self.basket_remove_item.nth(product_id)
+        remove_button.wait_for()
+        remove_button.click()
+
+        expect(self.basket_cards).to_have_count(before - 1)
--- test/test_basic.py
+++ test/test_basic.py
@@ -8,6 +8,10 @@ param = pytest.mark.parametrize
 
 
 def test_get_all_products(base_url, app: App):
+    """
+        1. go to products page
+        2. all products are existed on page
+    """
     app.products.visit(base_url)
     app.products.find_products(test_products)
 
@@ -16,10 +20,31 @@ def test_get_all_products(base_url, app: App):
 def test_add_product_to_basket(
         base_url, product_id: int, app: App
 ):
+    """
+        1. go to products page
+        2. add 1 product to basket
+        3. move to checkout
+    """
+    app.products.visit(base_url)
+    app.products.add_product_to_basket(product_id)
+
+    app.nav.go_to_checkout()
+
+
+@param("product_id", [1])
+def test_remove_product_from_basket(
+        base_url, product_id: int, app: App
+):
+    """
+        1. go to products page
+        2. add 1 product to basket
+        3. move to checkout
+        4. remove product from basket
+        5. product is removed
+    """
     app.products.visit(base_url)
     app.products.add_product_to_basket(product_id)
     app.nav.go_to_checkout()
 
-
-def test_remove_product_from_basket(base_url, app: App):
-    app.products.visit(base_url)
+    first = 0
+    app.checkout.remove_product_from_basket(first)
